d34e2dfc29b8c0e9aaf03953843a8761701ed385
Peter Golm <golm.peter@gmail.com>
2014-01-30 11:40:40 +0100

    remove trailing whitespace

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module DNSHub
 
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
-      
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
       
