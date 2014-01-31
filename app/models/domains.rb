0230f8711fb7ac5522bdbc10d19c525d48ce4c3d
Peter Golm <golm.peter@gmail.com>
2014-01-31 11:23:40 +0100

    add AnnotateModels - see: https://github.com/ctran/annotate_models

--- app/models/domains.rb
+++ app/models/domains.rb
@@ -1,3 +1,25 @@
+# ## Schema Information
+#
+# Table name: `domains`
+#
+# ### Columns
+#
+# Name                   | Type               | Attributes
+# ---------------------- | ------------------ | ---------------------------
+# **`id`**               | `integer`          | `not null, primary key`
+# **`name`**             | `string(255)`      | `not null`
+# **`master`**           | `string(128)`      |
+# **`last_check`**       | `integer`          |
+# **`type`**             | `string(6)`        | `not null`
+# **`notified_serial`**  | `integer`          |
+# **`account`**          | `string(40)`       |
+#
+# ### Indexes
+#
+# * `index_domains_on_name` (_unique_):
+#     * **`name`**
+#
+
 class Domains < ActiveRecord::Base
   include PDNS
   include DomainNameValidation
