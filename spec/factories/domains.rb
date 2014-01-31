40d9a57bdb097701f02477c9e1553893c74106ac
Peter Golm <golm.peter@gmail.com>
2014-01-31 13:48:51 +0100

    add validation to domain model

--- spec/factories/domains.rb
+++ spec/factories/domains.rb
@@ -2,11 +2,27 @@
 
 FactoryGirl.define do
   factory :domain do
-    name "MyString"
-    master "MyString"
+    id { generate(:random_id) }
+    name { "example#{id}.com" }
     last_check 1
-    type ""
     notified_serial 1
-    account "MyString"
+    account ""
+
+    trait :native do
+     type "NATIVE"
+    end
+
+    trait :master do
+     type "MASTER"
+    end
+
+    trait :slave do
+     type "SLAVE"
+     master { "192.168.1.#{(1..254).to_a.sample}" }
+    end
+
+    factory :native_domain, traits: [:native]
+    factory :master_domain, traits: [:master]
+    factory :slave_domain, traits: [:slave]
   end
 end
--- spec/models/domains_spec.rb
+++ spec/models/domains_spec.rb
@@ -1,5 +1,70 @@
 require 'spec_helper'
 
 describe Domain do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:domain) { build(:native_domain) }
+
+  it "can't be empty" do
+    domain.name = ""
+    expect(domain).not_to be_valid
+  end
+
+  it "accept valid domain" do
+    domain.name = "example.com"
+    expect(domain).to be_valid
+  end
+
+  it "denied dot terminated domain" do
+    domain.name = "example.com."
+    expect(domain).not_to be_valid
+  end
+end
+
+describe Domains, '#type' do
+  subject(:domain) { build(:domain) }
+
+  it "can't be empty" do
+    expect(domain).not_to be_valid
+  end
+
+  it "accept NATIVE" do
+    domain.type = "NATIVE"
+    expect(domain).to be_valid
+  end
+
+  it "accept MASTER" do
+    domain.type = "MASTER"
+    expect(domain).to be_valid
+  end
+
+  it "accept SLAVE" do
+    domain.type = "SLAVE"
+    domain.master = "127.0.0.1"
+    expect(domain).to be_valid
+  end
+
+  it "denied OTHER" do
+    domain.type = "OTHER"
+    expect(domain).not_to be_valid
+  end
+end
+
+describe Domains, '#master' do
+  subject(:slave_domain) { build(:slave_domain) }
+
+  context "when type is SLAVE" do
+    it "mustn't be empty" do
+      slave_domain.master = ""
+      expect(slave_domain).not_to be_valid
+    end
+
+    it "accept ip" do
+      slave_domain.master = "127.0.0.1"
+      expect(slave_domain).to be_valid
+    end
+
+    it "denied domains" do
+      slave_domain.master = "example.com"
+      expect(slave_domain).not_to be_valid
+    end
+  end
 end
